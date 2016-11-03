381ef256ce7b8490bcff806dace4183881d99a37
Hans Sowa <hanssowa@gmail.com>
2016-11-03 16:20:28 +0100

    Game starts with two fixed lands.

--- geography/src/main/java/game/geography/geography/Map.java
+++ geography/src/main/java/game/geography/geography/Map.java
@@ -6,19 +6,26 @@ public class Map implements LandOwnerChangeListener {
 
     private final HashMap<LandName, Land> landByName = new HashMap<>();
 
+    public Map() {
+        // in first version we have two lands
+        save(new Land(new LandName("Stormland"), this));
+        save(new Land(new LandName("Rainland"), this));
+    }
+
     public Land lookup(LandName landName) {
-        if (landByName.containsKey(landName)) {
-            return landByName.get(landName);
-        }
-
-        Land land = new Land(landName, this);
-        landByName.put(landName, land);
-
-        return landByName.get(landName);
+        return findById(landName);
     }
 
     @Override
     public void landOwnerHasChanged(Land land) {
+        save(land);
+    }
+
+    private Land findById(LandName landName) {
+        return landByName.get(landName);
+    }
+
+    private void save(Land land) {
         landByName.put(land.named(), land);
     }
 
