c97f9a709e4214865e5cfae84dbc471df4f74670
Hans Sowa <hanssowa@gmail.com>
2016-11-04 14:15:23 +0100

    Add TODOs.

--- rest/src/main/java/game/geography/rest/LandEndpoint.java
+++ rest/src/main/java/game/geography/rest/LandEndpoint.java
@@ -25,7 +25,7 @@ public class LandEndpoint {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getIt(@PathParam("landname") String landname, LandUpdate update) {
         Land land = map.lookup(new LandName(landname));
-        Owner newOwner = new Owner(new OwnerName(update.owner));
+        Owner newOwner = new Owner(new OwnerName(update.owner)); // TODO Factory methods?
 
         land.owned(newOwner);
 
