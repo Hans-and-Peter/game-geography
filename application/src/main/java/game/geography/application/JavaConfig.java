3f734ef55a0efb67fab15bf8fa6df1f75dba5296
Hans Sowa <hanssowa@gmail.com>
2016-11-04 12:50:20 +0100

    Add CDI config. Spring Boot runs at 8080.

---
+++ application/src/main/java/game/geography/application/JavaConfig.java
@@ -0,0 +1,21 @@
+package game.geography.application;
+
+import game.geography.geography.LandRepository;
+import game.geography.geography.Map;
+import game.geography.persistence.InMemoryLandRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JavaConfig {
+
+    @Bean
+    public LandRepository landRepository() {
+        return new InMemoryLandRepository();
+    }
+
+    @Bean
+    public Map map(LandRepository repository) {
+        return new Map(repository);
+    }
+}
