f478624ba029fe8da9d167885a90c6f0350db2b9
Hans Sowa <hanssowa@gmail.com>
2016-12-21 13:58:06 +0100

    Remove poetry mode with function calls.

--- bin/inc_version.rb
+++ bin/inc_version.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 
 def shell(command)
-  puts command
+  puts(command)
   stdout = `#{command}`
 
   error_code = $?.exitstatus
   if error_code != 0
-    puts stdout
-    raise "#{command} exit with error #{error_code}"
+    puts(stdout)
+    raise("#{command} exit with error #{error_code}")
   end
 
   stdout
@@ -20,15 +20,15 @@ mvn_stdout = shell(mvn_command)
 version_pattern = /^(\d+.*-)(\d)$/
 if mvn_stdout !~ version_pattern
   puts mvn_stdout
-  raise "version number #{version_pattern} not found"
+  raise("version number #{version_pattern} not found")
 end
-puts "found version #{$1}#{$2}"
+puts("found version #{$1}#{$2}")
 
 version = $1
 build = $2.to_i
 new_build = (build + 1).to_s
 new_version = version + new_build
 
-puts "will set new version #{new_version}"
+puts("will set new version #{new_version}")
 mvn_command = "mvn versions:set -DnewVersion=#{new_version}"
 shell(mvn_command)
