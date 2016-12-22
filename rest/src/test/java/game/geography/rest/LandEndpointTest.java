f65befa66531e38fdda6e16e4a95513c60ad3d87
Peter Kofler <peter.kofler@code-cop.org>
2016-12-22 23:50:35 +0100

    Add dummy test so Maven 2 does not fail on empty test.

--- rest/src/test/java/game/geography/rest/LandEndpointTest.java
+++ rest/src/test/java/game/geography/rest/LandEndpointTest.java
@@ -1,9 +1,18 @@
 package game.geography.rest;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class LandEndpointTest {
 
     // TODO test: add unit test for the LandEndpoint
 
     // @Mock
     // Map map;
+
+    @Test
+    public void shouldNotFailInMaven() {
+        assertTrue(true);
+    }
 }
