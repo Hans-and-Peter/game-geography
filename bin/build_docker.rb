1c48a571f54e332c0801b3c1a69507453ac4f503
Hans Sowa <hanssowa@gmail.com>
2016-12-22 16:00:48 +0100

    Document SnapCI settings.

--- bin/build_docker.rb
+++ bin/build_docker.rb
@@ -4,6 +4,7 @@ require_relative 'lib/shell.rb'
 Dir.chdir('application')
 
 puts "will create docker image"
+# Maven runs docker build -f application/src/main/docker/Dockerfile -t hans-and-peter.game.geography:1.0.0-2 application/
 mvn_command = 'mvn docker:build'
 shell(mvn_command)
 
