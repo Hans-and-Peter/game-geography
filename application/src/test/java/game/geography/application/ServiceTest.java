00d120ba5e66d8fcb88248a068499f66135e43af
Hans Sowa <hanssowa@gmail.com>
2016-12-22 14:06:39 +0100

    Comment.

--- application/src/test/java/game/geography/application/ServiceTest.java
+++ application/src/test/java/game/geography/application/ServiceTest.java
@@ -12,11 +12,14 @@ import java.util.Map;
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.core.Is.is;
 
+/**
+ * Tests the whole service. This is a system test of the service (an integration test).
+ */
 // see http://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-testing.html
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ServiceTest {
-    // Tests the whole service. This is a system test of the service (an integration test).
+    // TODO naming: find a better name of test, based on service entry points? under test
 
     @LocalServerPort
     private int endpointPort;
