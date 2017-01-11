850c332ffc4581bd57b307d42a929ece6ca9fba1
Peter Kofler <peter.kofler@code-cop.org>
2017-01-11 22:33:46 +0100

    Change wildcards in Grep so it works on my machine as well.

--- find_open_issues.sh
+++ find_open_issues.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-grep -r -i --include \*.java --include \*.xml --include \*.txt --include \*.properties --include Dockerfile --exclude workspace.xml TODO .
+grep -r -i --include "*.java" --include "*.xml" --include "*.txt" --include "*.properties" --include Dockerfile --exclude workspace.xml TODO .
