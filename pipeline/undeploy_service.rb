f4248db44f1970e1f2a58bc22dea26d1d2ad986b
Hans Sowa <hanssowa@gmail.com>
2017-02-02 16:30:21 +0100

    Extract undeploy of service to Ruby script.

---
+++ pipeline/undeploy_service.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+require_relative 'lib/shell.rb'
+
+if ARGV.length == 0
+  raise "need to pass service_name"
+end
+service_name = ARGV[0]
+
+remove_task_cmd = "aws ecs update-service --service \"#{service_name}\" --desired-count 0"
+shell(remove_task_cmd)
+
+delete_service_cmd = "aws ecs delete-service --service \"#{service_name}\""
+SHELL(delete_service_cmd)
+
+# aws ecs update-service --service "${serviceName}" --desired-count 0 > dev/nul
+# aws ecs delete-service --service "${serviceName}" > dev/nul
