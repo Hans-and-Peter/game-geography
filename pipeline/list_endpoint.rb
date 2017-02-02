348878302cadb22e3d1731c8a198a5f0ca7b9905
Hans Sowa <hanssowa@gmail.com>
2017-02-02 16:59:11 +0100

    Extract list endpoint to Ruby script.

--- pipeline/list_endpoint.rb
+++ pipeline/list_endpoint.rb
@@ -10,7 +10,8 @@ service_name = ARGV[0]
 wait_service_cmd = "aws ecs wait services-stable --services \"#{service_name}\""
 shell(wait_service_cmd)
 
-list_task_cmd = "aws ecs list-tasks --service-name \"#{service_name}\" --output text --query 'taskArns'"
+list_task_cmd = "aws ecs list-tasks --service-name \"#{service_name}\" " +
+    "--output text --query 'taskArns'"
 task_id = shell(list_task_cmd)
 
 wait_task_cmd = "aws ecs wait tasks-running --tasks \"#{task_id}\""
@@ -21,12 +22,16 @@ desc_task_cmd = "aws ecs describe-tasks --tasks \"#{task_id}\" " +
     "--output text --query 'tasks[*].containerInstanceArn'"
 task_container_id = shell(desc_task_cmd)
 
-export task_instance_id=$( aws ecs describe-container-instances --container-instances "$task_container_id" \
---output text --query 'containerInstances[*].ec2InstanceId' )
-# get public ip of container instance
-export public_host=$( aws ec2 describe-instances --instance-id $task_instance_id \
---output text --query 'Reservations[*].Instances[*].NetworkInterfaces[*].Association.PublicDnsName' )
+describe_instance_cmd = "aws ecs describe-container-instances --container-instances \"#{task_container_id}\" " +
+    "--output text --query 'containerInstances[*].ec2InstanceId'"
+task_instance_id = shell(describe_instance_cmd)
 
+# get public ip of container instance
+describe_instance_cmd = "aws ec2 describe-instances --instance-id \"#{task_instance_id}\" "+
+    "--output text --query 'Reservations[*].Instances[*].NetworkInterfaces[*].Association.PublicDnsName'"
+public_host = shell(describe_instance_cmd)
+
+puts public_host
 
 # # wait for task to become active
 # aws ecs wait services-stable --services "$serviceName"
