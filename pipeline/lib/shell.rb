b66b3c152dfb75dfbdeb745178be011409eca1f2
Hans Sowa <hanssowa@gmail.com>
2017-02-02 17:21:15 +0100

    Show command in StdErr so it is visible even inside $().

--- pipeline/lib/shell.rb
+++ pipeline/lib/shell.rb
@@ -1,12 +1,13 @@
 #!/usr/bin/env ruby
 
 def shell(command)
+  STDERR.puts command
   stdout = `#{command}`
 
   error_code = $?.exitstatus
   if error_code != 0
-    puts command
-    puts stdout
+    STDERR.puts error_code
+    STDERR.puts stdout
     raise "#{command} exit with error #{error_code}"
   end
 
